4430cddf55b2f71802b454ab8b171badf283f6f2
iitsoftware <am@iit.de>
2019-04-02 12:00:10 +0200

    -Dnashorn.args="--no-deprecation-warning"

--- distribution/disttemplates/router/scripts/router.bat
+++ distribution/disttemplates/router/scripts/router.bat
@@ -3,4 +3,4 @@ IF "%1"=="java9" (
   set OPENS=--add-opens=java.desktop/java.awt.font=ALL-UNNAMED --add-exports=java.desktop/com.sun.awt=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED
   shift
 )
-java -server -Xmx1024M %OPENS% -cp ../jars/swiftmq.jar com.swiftmq.Router ../config/routerconfig.xml %1
+java -server -Xmx1024M %OPENS% -cp ../jars/swiftmq.jar -Dnashorn.args="--no-deprecation-warning" com.swiftmq.Router ../config/routerconfig.xml %1
