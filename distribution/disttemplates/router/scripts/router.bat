f04c7f02a1bce6e928687f199f1bc8b80a3a8ef6
iitsoftware <am@iit.de>
2020-03-19 11:40:04 +0100

    fix router.bat preconfig

--- distribution/disttemplates/router/scripts/router.bat
+++ distribution/disttemplates/router/scripts/router.bat
@@ -4,7 +4,7 @@ IF "%1"=="java9" (
   set OPENS=--add-opens=java.desktop/java.awt.font=ALL-UNNAMED --add-exports=java.desktop/com.sun.awt=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED
   shift
 )
-IF "%1"=="" (
+IF NOT "%1"=="" (
   set PRECONFIG=%PRECONFIG%,%1
 )
 java -server -Xmx1024M %OPENS% %PRECONFIG% -Dswiftmq.directory.autocreate=true -Djavax.net.ssl.keyStore=../certs/server.keystore -Djavax.net.ssl.keyStorePassword=secret -Djavax.net.ssl.trustStore=../certs/client.truststore -Djavax.net.ssl.trustStorePassword=secret -cp ../jars/swiftmq.jar -Dnashorn.args="--no-deprecation-warning" com.swiftmq.Router ../data/config/routerconfig.xml
