3b5fa416f4868eecda0116f03064aeb2adc20394
iitsoftware <am@iit.de>
2020-03-27 10:31:28 +0100

    switch to graalvm

--- distribution/disttemplates/router/scripts/router.bat
+++ distribution/disttemplates/router/scripts/router.bat
@@ -1,10 +1,16 @@
 set OPENS=
 set PRECONFIG=-Dswiftmq.preconfig=../data/preconfig/upgrade-to-12.1.0.xml
+set GRAAL=false
 IF "%1"=="java9" (
+  set OPENS=--add-opens=java.desktop/java.awt.font=ALL-UNNAMED --add-exports=java.desktop/com.sun.awt=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Dnashorn.args=--no-deprecation-warning
+  shift
+)
+IF "%1"=="graal" (
   set OPENS=--add-opens=java.desktop/java.awt.font=ALL-UNNAMED --add-exports=java.desktop/com.sun.awt=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED
+  set GRAAL=true
   shift
 )
 IF NOT %1=="" (
   set PRECONFIG=%PRECONFIG%,%⁓1
 )
-java -server -Xmx1024M %OPENS% %PRECONFIG% -Dswiftmq.directory.autocreate=true -Djavax.net.ssl.keyStore=../certs/server.keystore -Djavax.net.ssl.keyStorePassword=secret -Djavax.net.ssl.trustStore=../certs/client.truststore -Djavax.net.ssl.trustStorePassword=secret -cp ../jars/swiftmq.jar -Dnashorn.args="--no-deprecation-warning" com.swiftmq.Router ../data/config/routerconfig.xml
+java -server -Xmx1024M %OPENS% %PRECONFIG% -Dswiftmq.graalvm=%GRAAL% -Dswiftmq.directory.autocreate=true -Djavax.net.ssl.keyStore=../certs/server.keystore -Djavax.net.ssl.keyStorePassword=secret -Djavax.net.ssl.trustStore=../certs/client.truststore -Djavax.net.ssl.trustStorePassword=secret -cp ../jars/swiftmq.jar -Dnashorn.args="--no-deprecation-warning" com.swiftmq.Router ../data/config/routerconfig.xml
