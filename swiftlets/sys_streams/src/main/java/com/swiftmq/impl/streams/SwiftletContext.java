3b0d9d85c4750f7630020392e9d207b2994ef2f6
iitsoftware <am@iit.de>
2020-04-24 10:27:41 +0200

    auto check on GraalVM via java.vm.name

--- swiftlets/sys_streams/src/main/java/com/swiftmq/impl/streams/SwiftletContext.java
+++ swiftlets/sys_streams/src/main/java/com/swiftmq/impl/streams/SwiftletContext.java
@@ -77,7 +77,7 @@ public class SwiftletContext {
         jndiSwiftlet = (JNDISwiftlet) SwiftletManager.getInstance().getSwiftlet("sys$jndi");
         storeSwiftlet = (StoreSwiftlet) SwiftletManager.getInstance().getSwiftlet("sys$store");
         threadpoolSwiftlet = (ThreadpoolSwiftlet) SwiftletManager.getInstance().getSwiftlet("sys$threadpool");
-        ISGRAAL = System.getProperty("java.vm.version") != null && System.getProperty("java.vm.version").contains("GraalVM");
-        logSwiftlet.logInformation(streamsSwiftlet.getName(), "java.vm.version: " + System.getProperty("java.vm.version") + ", runnung on GraalVM: " + ISGRAAL);
+        ISGRAAL = System.getProperty("java.vm.name") != null && System.getProperty("java.vm.name").contains("GraalVM");
+        logSwiftlet.logInformation(streamsSwiftlet.getName(), "java.vm.name: " + System.getProperty("java.vm.name") + ", runnung on GraalVM: " + ISGRAAL);
     }
 }
