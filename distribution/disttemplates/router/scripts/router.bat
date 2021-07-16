ef15bfa779a8a6133adf8e5d88a8ce54f30c3874
iitsoftware <am@iit.de>
2021-07-16 11:43:37 +0200

    Check Java 11 in scripts instead from Java

--- distribution/disttemplates/router/scripts/router.bat
+++ distribution/disttemplates/router/scripts/router.bat
@@ -1,16 +1,16 @@
 @echo off
 where java >nul 2>nul
-if %errorlevel% == 1 (
+if %errorlevel%==1 (
     @echo Please install Java 11 or later before using SwiftMQ! From Java 15 onwards you need GraalVM: https://graalvm.org
     exit
 )
 
-for /f "tokens=*" %%i in ('java -cp ../jars/swiftmq.jar com.swiftmq.JavaHome') do set JAVAHOME=%%i
-for /f %%i in ('java -cp ../jars/swiftmq.jar com.swiftmq.IsJava11') do set ISJAVA11=%%i
-if "%ISJAVA11%" == "false" (
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j"
+if %jver% LSS 11 (
     @echo Please install Java 11 or later before using SwiftMQ! From Java 15 onwards you need GraalVM: https://graalvm.org
     exit
 )
+for /f "tokens=*" %%i in ('java -cp ../jars/swiftmq.jar com.swiftmq.JavaHome') do set JAVAHOME=%%i
 set OPENS=--add-opens=java.desktop/java.awt.font=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED  --add-opens=java.base/sun.nio.ch=ALL-UNNAMED  --add-opens=java.base/sun.net.dns=ALL-UNNAMED -Dnashorn.args=--no-deprecation-warning
 
 set PRECONFIG=-Dswiftmq.preconfig=../data/preconfig/upgrade-to-12.1.0.xml
