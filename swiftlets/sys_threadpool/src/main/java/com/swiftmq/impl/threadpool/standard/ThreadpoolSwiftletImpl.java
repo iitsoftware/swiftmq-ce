c5d7a5d1382d4e302db94bfd9e2f9892618755c3
iitsoftware <am@iit.de>
2023-12-22 13:46:10 +0100

    sys_threadpool: removed old thread pools

--- swiftlets/sys_threadpool/src/main/java/com/swiftmq/impl/threadpool/standard/ThreadpoolSwiftletImpl.java
+++ swiftlets/sys_threadpool/src/main/java/com/swiftmq/impl/threadpool/standard/ThreadpoolSwiftletImpl.java
@@ -21,45 +21,40 @@ import com.swiftmq.impl.threadpool.standard.group.EventLoopImpl;
 import com.swiftmq.impl.threadpool.standard.group.GroupRegistry;
 import com.swiftmq.impl.threadpool.standard.group.pool.ThreadRunner;
 import com.swiftmq.impl.threadpool.standard.group.pool.VirtualThreadRunner;
-import com.swiftmq.mgmt.*;
+import com.swiftmq.mgmt.Configuration;
+import com.swiftmq.mgmt.Property;
+import com.swiftmq.mgmt.PropertyChangeAdapter;
 import com.swiftmq.swiftlet.SwiftletException;
 import com.swiftmq.swiftlet.SwiftletManager;
 import com.swiftmq.swiftlet.event.SwiftletManagerAdapter;
 import com.swiftmq.swiftlet.event.SwiftletManagerEvent;
 import com.swiftmq.swiftlet.mgmt.MgmtSwiftlet;
 import com.swiftmq.swiftlet.mgmt.event.MgmtListener;
-import com.swiftmq.swiftlet.threadpool.*;
+import com.swiftmq.swiftlet.threadpool.EventLoop;
+import com.swiftmq.swiftlet.threadpool.EventProcessor;
+import com.swiftmq.swiftlet.threadpool.ThreadpoolSwiftlet;
 import com.swiftmq.swiftlet.timer.TimerSwiftlet;
 import com.swiftmq.swiftlet.timer.event.TimerListener;
-import com.swiftmq.tools.sql.LikeComparator;
 
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ThreadpoolSwiftletImpl extends ThreadpoolSwiftlet
         implements TimerListener {
-    public static final String PROP_KERNEL_POOL = "kernel-pool";
     public static final String PROP_COLLECT_INTERVAL = "collect-interval";
     public static final String PROP_PLATFORM_THREADS = "01platform";
     public static final String PROP_VIRTUAL_THREADS = "02virtual";
     public static final String PROP_ADHOC_THREADS = "03adhoc";
-    public static final String DEFAULT_POOL = "default";
 
     SwiftletContext ctx = null;
-    Map<String, ThreadPool> pools = new ConcurrentHashMap<>();
-    Map<String, String> threadNameMaps = new ConcurrentHashMap<>();
     GroupRegistry groupRegistry = null;
     ThreadRunner adHocVirtualThreadRunner = null;
 
     final AtomicBoolean collectOn = new AtomicBoolean(false);
     final AtomicLong collectInterval = new AtomicLong(-1);
-    final AtomicBoolean stopped = new AtomicBoolean(false);
 
     private void collectChanged(long oldInterval, long newInterval) {
         if (!collectOn.get())
@@ -88,21 +83,6 @@ public class ThreadpoolSwiftletImpl extends ThreadpoolSwiftlet
         EventLoopImpl eventLoop = new EventLoopImpl(id, groupRegistry.isBulkMode(id), eventProcessor, groupRegistry.threadRunnerForEventLoop(id));
         groupRegistry.getGroup(id).addEventLoop(eventLoop);
         return eventLoop;
-    }
-
-    private String[] getDefinedPoolnames(EntityList list) {
-        if (list.getEntities().isEmpty())
-            return null;
-        return (String[]) list.getEntities().keySet().toArray(new String[0]);
-    }
-
-    private void storeThreadNamesForPool(String poolname, Entity poolEntity) {
-        EntityList list = (EntityList) poolEntity.getEntity("threads");
-        Map m = list.getEntities();
-        if (!m.isEmpty()) {
-            for (Object o : m.keySet())
-                threadNameMaps.put((String) o, poolname);
-        }
     }
 
     public void performTimeAction() {
@@ -138,177 +118,12 @@ public class ThreadpoolSwiftletImpl extends ThreadpoolSwiftlet
         if (ctx.traceSpace.enabled) ctx.traceSpace.trace(getName(), "collecting thread counts...DONE.");
     }
 
-    public String[] getPoolnames() {
-        return pools.keySet().toArray(new String[0]);
-    }
-
-    public ThreadPool getPoolByName(String name) {
-        return pools.get(name);
-    }
-
-    public ThreadPool getPool(String threadName) {
-        String name = DEFAULT_POOL;
-        for (String predicate : threadNameMaps.keySet()) {
-            if (LikeComparator.compare(threadName, predicate, '\\')) {
-                name = threadNameMaps.get(predicate);
-                break;
-            }
-        }
-        ThreadPool p = pools.get(name);
-        if (ctx.traceSpace.enabled)
-            ctx.traceSpace.trace(getName(), "getPoolForThreadName '" + threadName + "' returns " + name);
-        return p;
-    }
-
-    public void dispatchTask(AsyncTask asyncTask) {
-        ThreadPool pool = getPool(asyncTask.getDispatchToken());
-        if (pool == null)
-            return; // only during shutdown
-        if (ctx.traceSpace.enabled)
-            ctx.traceSpace.trace(getName(), "dispatchTask, dispatchToken=" + asyncTask.getDispatchToken() +
-                    ", description=" + asyncTask.getDescription() +
-                    ", pool=" + pool.getPoolName());
-        pool.dispatchTask(asyncTask);
-    }
-
-    public void stopPools() {
-        if (stopped.getAndSet(true))
-            return;
-        for (Map.Entry<String, ThreadPool> stringThreadPoolEntry : pools.entrySet()) {
-            ThreadPool p = (ThreadPool) ((Map.Entry<?, ?>) stringThreadPoolEntry).getValue();
-            p.stop();
-        }
-    }
-
-    private void createPoolChangeListeners(PoolDispatcher pool, Entity poolEntity) {
-        Property prop = poolEntity.getProperty("max-threads");
-        prop.setPropertyChangeListener(new PropertyChangeAdapter(pool) {
-
-            public void propertyChanged(Property property, Object oldValue, Object newValue) throws PropertyChangeException {
-                PoolDispatcher p = (PoolDispatcher) configObject;
-                int n = (Integer) newValue;
-                if (n < p.getMinThreads())
-                    throw new PropertyChangeException("max-threads must be greater or equal to min-threads");
-                p.setMaxThreads(n);
-            }
-        });
-        prop = poolEntity.getProperty("queue-length-threshold");
-        prop.setPropertyChangeListener(new PropertyChangeAdapter(pool) {
-
-            public void propertyChanged(Property property, Object oldValue, Object newValue) throws PropertyChangeException {
-                PoolDispatcher p = (PoolDispatcher) configObject;
-                int n = (Integer) newValue;
-                p.setThreshold(n);
-            }
-        });
-        prop = poolEntity.getProperty("additional-threads");
-        prop.setPropertyChangeListener(new PropertyChangeAdapter(pool) {
-
-            public void propertyChanged(Property property, Object oldValue, Object newValue) throws PropertyChangeException {
-                PoolDispatcher p = (PoolDispatcher) configObject;
-                int n = (Integer) newValue;
-                p.setAddThreads(n);
-            }
-        });
-    }
-
-    private void createPool(String poolName, Entity poolEntity, Entity defaultEntity) {
-        Entity entity = poolEntity != null ? poolEntity : defaultEntity;
-        boolean kernelPool = (Boolean) entity.getProperty("kernel-pool").getValue();
-        int min = (Integer) entity.getProperty("min-threads").getValue();
-        int max = (Integer) entity.getProperty("max-threads").getValue();
-        int threshold = (Integer) entity.getProperty("queue-length-threshold").getValue();
-        int addThreads = (Integer) entity.getProperty("additional-threads").getValue();
-        int prio = (Integer) entity.getProperty("priority").getValue();
-        long ttl = (Long) entity.getProperty("idle-timeout").getValue();
-        if (ctx.traceSpace.enabled)
-            ctx.traceSpace.trace(getName(), "creating thread pool '" + poolName +
-                    "', kernelPool=" + kernelPool +
-                    ", minThreads=" + min +
-                    ", maxThreads=" + max +
-                    ", threshold=" + threshold +
-                    ", addThreads=" + addThreads +
-                    ", prio=" + prio +
-                    ", idletimeout=" + ttl);
-        PoolDispatcher pool = new PoolDispatcher(getName(), poolName, kernelPool, min, max, threshold, addThreads, prio, ttl);
-        pools.put(poolName, pool);
-        createPoolChangeListeners(pool, entity);
-        if (!poolName.equals(DEFAULT_POOL)) {
-            EntityList list = (EntityList) poolEntity.getEntity("threads");
-            list.setEntityAddListener(new EntityChangeAdapter(pool) {
-                public void onEntityAdd(Entity parent, Entity newEntity)
-                        throws EntityAddException {
-                    PoolDispatcher myPd = (PoolDispatcher) configObject;
-                    if (myPd.isKernelPool())
-                        throw new EntityAddException("You cannot create a thread assignment for a kernel pool dynamically.");
-                    threadNameMaps.put(newEntity.getName(), myPd.getPoolName());
-                    if (ctx.traceSpace.enabled)
-                        ctx.traceSpace.trace(getName(), "onEntityAdd (thread), poolName=" + myPd.getPoolName() + ", thread=" + newEntity.getName());
-                }
-            });
-            list.setEntityRemoveListener(new EntityChangeAdapter(pool) {
-                public void onEntityRemove(Entity parent, Entity delEntity)
-                        throws EntityRemoveException {
-                    PoolDispatcher myPd = (PoolDispatcher) configObject;
-                    if (myPd.isKernelPool())
-                        throw new EntityRemoveException("You cannot remove a thread assignment from a kernel pool dynamically.");
-                    threadNameMaps.remove(delEntity.getName());
-                    if (ctx.traceSpace.enabled)
-                        ctx.traceSpace.trace(getName(), "onEntityRemove (thread), poolName=" + myPd.getPoolName() + ", thread=" + delEntity.getName());
-                }
-            });
-        }
-    }
-
     protected void startup(Configuration config) throws SwiftletException {
         ctx = new SwiftletContext(config, this);
         if (ctx.traceSpace.enabled) ctx.traceSpace.trace(getName(), "startup ...");
 
         groupRegistry = new GroupRegistry(ctx);
         adHocVirtualThreadRunner = new VirtualThreadRunner();
-
-        EntityList poolList = (EntityList) ctx.config.getEntity("pools");
-        createPool(DEFAULT_POOL, null, poolList.getTemplate());
-        PoolDispatcher dp = (PoolDispatcher) pools.get(DEFAULT_POOL);
-        dp.setKernelPool(true);
-
-        String[] poolNames = getDefinedPoolnames(poolList);
-        if (poolNames != null && poolNames.length > 0) {
-            if (ctx.traceSpace.enabled) ctx.traceSpace.trace(getName(), "startup: starting defined thread pools");
-            for (String poolName : poolNames) {
-                createPool(poolName, poolList.getEntity(poolName), poolList.getTemplate());
-                storeThreadNamesForPool(poolName, poolList.getEntity(poolName));
-            }
-        }
-        poolList.setEntityAddListener(new EntityChangeAdapter(null) {
-            public void onEntityAdd(Entity parent, Entity newEntity)
-                    throws EntityAddException {
-                boolean kp = (Boolean) newEntity.getProperty(PROP_KERNEL_POOL).getValue();
-                if (kp)
-                    throw new EntityAddException("You cannot create a kernel pool dynamically.");
-                createPool(newEntity.getName(), newEntity, newEntity);
-                if (ctx.traceSpace.enabled)
-                    ctx.traceSpace.trace(getName(), "onEntityAdd (pool), poolName=" + newEntity.getName());
-            }
-        });
-        poolList.setEntityRemoveListener(new EntityChangeAdapter(null) {
-            public void onEntityRemove(Entity parent, Entity delEntity)
-                    throws EntityRemoveException {
-                PoolDispatcher pd = (PoolDispatcher) pools.get(delEntity.getName());
-                if (pd.isKernelPool())
-                    throw new EntityRemoveException("You cannot remove a kernel pool dynamically.");
-                pd.close();
-                pools.remove(delEntity.getName());
-                poolList.removeDynamicEntity(pd);
-                for (Iterator<Map.Entry<String, String>> iter = threadNameMaps.entrySet().iterator(); iter.hasNext(); ) {
-                    String entry = (String) ((Map.Entry<?, ?>) iter.next()).getValue();
-                    if (entry.equals(pd.getPoolName()))
-                        iter.remove();
-                }
-                if (ctx.traceSpace.enabled)
-                    ctx.traceSpace.trace(getName(), "onEntityRemove (pool): poolName=" + delEntity.getName());
-            }
-        });
 
         try {
             SwiftletManager.getInstance().addSwiftletManagerListener("sys$mgmt", new SwiftletManagerAdapter() {
@@ -339,8 +154,7 @@ public class ThreadpoolSwiftletImpl extends ThreadpoolSwiftlet
 
         Property prop = ctx.config.getProperty(PROP_COLLECT_INTERVAL);
         prop.setPropertyChangeListener(new PropertyChangeAdapter(null) {
-            public void propertyChanged(Property property, Object oldValue, Object newValue)
-                    throws PropertyChangeException {
+            public void propertyChanged(Property property, Object oldValue, Object newValue) {
                 collectInterval.set((Long) newValue);
                 collectChanged((Long) oldValue, collectInterval.get());
             }
@@ -360,12 +174,6 @@ public class ThreadpoolSwiftletImpl extends ThreadpoolSwiftlet
         if (ctx == null)
             return;
         if (ctx.traceSpace.enabled) ctx.traceSpace.trace(getName(), "shutdown: closing thread pools ...");
-        for (Map.Entry<String, ThreadPool> entry : pools.entrySet()) {
-            ThreadPool p = (ThreadPool) ((Map.Entry<?, ?>) entry).getValue();
-            p.close();
-        }
-        pools.clear();
-        threadNameMaps.clear();
         ctx.logSwiftlet.logInformation(getName(), "/shutdown/adHocVirtualThreadRunner shutdown");
         adHocVirtualThreadRunner.shutdown(10, TimeUnit.SECONDS);
         ctx.logSwiftlet.logInformation(getName(), "/shutdown/adHocVirtualThreadRunner shutdown done");
