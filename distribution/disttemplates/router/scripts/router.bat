49397a11f384ad8a4960fb3d650514dc9bf381bd
iitsoftware <am@iit.de>
2024-01-13 12:19:32 +0100

    dynamic sorted preconfig files

--- distribution/disttemplates/router/scripts/router.bat
+++ distribution/disttemplates/router/scripts/router.bat
@@ -6,15 +6,29 @@ set /p JAVA_HOME=<.javahome
 set OPENS=--module-path=../graalvm --add-modules=org.graalvm.polyglot --add-opens=java.desktop/java.awt.font=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED  --add-opens=java.base/sun.nio.ch=ALL-UNNAMED  --add-opens=java.base/sun.net.dns=ALL-UNNAMED -Dnashorn.args=--no-deprecation-warning
 
 set "PRECONFIG=-Dswiftmq.preconfig="
+set "TEMP_FILE=tempfile.txt"
 
+:: Clear the temp file
+type nul > %TEMP_FILE%
+
+:: Accumulate file names
 for %%F in (..\data\preconfig\*.xml) do (
-    if defined TEMP_PRECONFIG (
-        set "PRECONFIG=%PRECONFIG%,%%F"
-    ) else (
-        set "TEMP_PRECONFIG=1"
-        set "PRECONFIG=%PRECONFIG%%%F"
-    )
+    echo %%F >> %TEMP_FILE%
 )
+
+:: Read the temp file and build the PRECONFIG string
+set /p PRECONFIG=%PRECONFIG%<%TEMP_FILE%
+for /f "skip=1 delims=" %%A in (%TEMP_FILE%) do (
+    set /p =,%PRECONFIG%%%A< nul >> %TEMP_FILE%
+    set /p PRECONFIG=<%TEMP_FILE%
+)
+
+:: Display the result
+echo %PRECONFIG%
+
+:: Clean up the temporary file
+del %TEMP_FILE%
+
 IF NOT "%~1"=="" (
   set PRECONFIG=%PRECONFIG%,%~1
 )
